ebe06c5b6673263d9eb21af097e840cfe1a21a10
Ondrej Grover <ondrej.grover@gmail.com>
2012-06-04 13:36:28 +0200

    distribution stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name="pyAC250Kxxx",
+      version='1.0',
+      description="Python API and command-line program for serial port communication with an AC250Kxxx series Diametral power source",
+      author="Ondrej Grover",
+      author_email="ondrej.grover@gmail.com",
+      url="http://github.com/smartass101/pyAC250Kxx",
+      download_url="https://github.com/smartass101/pyAC250Kxxx/zipball/master",
+      py_modules=['pyAC205Kxxx'],
+      scripts=['AC205K'],
+      )
